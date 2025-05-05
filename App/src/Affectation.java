47f85b78e61446da5c7529337291d40b8041f516
Antonin Marouze <antonin.marouze.etu@univ-lille.fr>
2025-05-05 19:10:30 +0200

    POO::DefClasse

---
+++ App/src/Affectation.java
@@ -0,0 +1,10 @@
+public class Affectation {
+    private double comptability;
+    private Teenagers host;
+    private Teenagers visitor;
+
+    public Affectation(Teenagers host,Teenagers visitor){
+        this.host=host;
+        this.visitor=visitor;
+    }
+}
---
+++ App/src/Country.java
@@ -0,0 +1,3 @@
+public enum Country {
+    FR,GE,IT,ES,
+}
